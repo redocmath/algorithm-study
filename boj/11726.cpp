d38ebaeca61ac379345da4be787ef931db6734c4
RedoC-github <coderlovemedia@gmail.com>
2021-01-27 22:50:30 +0900

    boj: 11726

--- boj/11726.cpp
+++ boj/11726.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define FOR(i, start, end) for (int i = (start); i < (end); i++)
 #define MAX 1000
 
 using namespace std;
 
 int N;
-int cache[MAX];
-
-int getCases(int width) {
-    int& tmp = cache[width];
-
-    if (tmp != -1) return tmp;
-    return tmp = (getCases(width - 1) + getCases(width - 2)) % 10007;
-}
+int cache[MAX + 1];
 
 int main() {
-    FOR(i, 0, MAX) {
-        cache[i] = -1;
-    }
     cache[1] = 1;
     cache[2] = 2;
 
     ios::sync_with_stdio(false);
     cin >> N;
-    cout << getCases(N);
+
+    // bottom-up
+    for (int i = 3; i <= N; i++) {
+        cache[i] = (cache[i - 1] + cache[i - 2]) % 10007;
+    } 
+
+    cout << cache[N] << endl;
     return 0;
 }
