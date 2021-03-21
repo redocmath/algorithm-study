872df0c06b39e28adb5f39d1eacab1e6f27f7023
RedoC-github <coderlovemedia@gmail.com>
2021-03-21 22:49:59 +0900

    whoo

--- boj/5585.cpp
+++ boj/5585.cpp
@@ -14,8 +14,8 @@ int main() {
     cin.tie(nullptr); cout.tie(nullptr);
     int n; cin >> n; n = 1000 - n;
     int cnt = 0;
-    const int d[5] = {500, 100, 50, 10, 5, 1};
-    for (int i = 0; i < 5; i++) {
+    const int d[6] = {500, 100, 50, 10, 5, 1};
+    for (int i = 0; i < 6; i++) {
         cnt += n / d[i]; n %= d[i];
     }
     cout << cnt;
