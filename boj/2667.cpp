dc34bf5839980e2260fc39a4f80f96a7141de86a
RedoC-github <coderlovemedia@gmail.com>
2021-03-25 23:28:26 +0900

    boj: 2667

--- boj/2667.cpp
+++ boj/2667.cpp
@@ -2,57 +2,56 @@
 typedef long long ll;
 using namespace std;
 
-int board[25][25];
-bool vis[25][25];
-
-int n;
 
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
 int main() {
     ios::sync_with_stdio(false);
-    int n; cin >> n;
-    for (int i = 0; i < n; i++) {
-        string a; cin >> a;
-        for (int j = 0; j < n; j++) {
-            board[i][j] = (a[j] == '0' ? 0 : 1);
+    int t; cin >> t;
+    while (t--) {
+        int board[50][50];
+        bool vis[50][50];
+
+        int m, n, k; cin >> m >> n >> k;
+
+        for (int i = 0; i < n; i++) 
+            fill(board[i], board[i]+m, 0);
+
+        for (int i = 0; i < n; i++) 
+            fill(vis[i], vis[i]+m, false);
+
+        for (int i = 0; i < k; i++) {
+            int a, b; cin >> a >> b;
+            board[b][a] = 1;
         }
-    }
 
-    int cnt = 0; vector<int> tmp;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (board[i][j] && !vis[i][j]) {
-                queue<pair<int, int>> Q;
-                vis[i][j] = true;
-                Q.push({i, j});
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] && !vis[i][j]) {
+                    queue<pair<int, int>> Q;
+                    vis[i][j] = true;
+                    Q.push({i, j});
 
-                int tmpp = 0;
-                while (!Q.empty()) {
-                    tmpp++;
-                    pair<int, int> cur = Q.front(); Q.pop();
-                    for (int k = 0; k < 4; k++) {
-                        int nx = cur.first + dx[k];
-                        int ny = cur.second + dy[k];
+                    while (!Q.empty()) {
+                        pair<int, int> cur = Q.front(); Q.pop();
+                        for (int k = 0; k < 4; k++) {
+                            int ny = cur.first + dy[k];
+                            int nx = cur.second + dx[k];
 
-                        if(nx < 0 || nx >= n || ny < 0 || ny >= n) continue;  
-                        if(vis[nx][ny] || board[nx][ny] != 1) continue; 
+                            if(nx < 0 || nx >= m || ny < 0 || ny >= n) continue;  
+                            if(vis[ny][nx] || board[ny][nx] != 1) continue; 
 
-                        vis[nx][ny] = true;
-                        Q.push({nx,ny});
+                            vis[ny][nx] = true;
+                            Q.push({ny,nx});
+                        }
                     }
+                    cnt++;
                 }
-                cnt++;
-                tmp.push_back(tmpp);
             }
         }
-    }
-
-    sort(tmp.begin(), tmp.end());
-    cout << cnt << "\n";
-    for (int i = 0; i < cnt; i++) {
-        cout << tmp[i] << "\n";
+        cout << cnt << "\n";
     }
     return 0;
 }
