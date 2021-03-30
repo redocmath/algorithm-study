8018710fd60b9964dd6833b27953c22b7d7b0fce
RedoC-github <coderlovemedia@gmail.com>
2021-03-30 17:25:32 +0900

    ...

---
+++ boj/18258.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n; cin >> n;
+    queue<int> S;
+
+    while (n--) {
+        string s; cin >> s;
+        if (s == "push") {
+            int a; cin >> a;
+            S.push(a);
+        }
+        else if (s == "pop") {
+            if (S.empty()) {
+                cout << "-1\n";
+            } else {
+                cout << S.front() << "\n";
+                S.pop();
+            }
+            
+        }
+        else if (s == "size") {
+            cout << S.size() << "\n";
+        }
+        else if (s == "empty") {
+            cout << S.empty() << "\n";
+        } else if (s == "front"){
+            if (S.empty()) cout << "-1\n";
+            else cout << S.front() << "\n";
+        } else if (s == "back") {
+            if (S.empty()) cout << "-1\n";
+            else cout << S.back() << "\n";
+        }
+    }
+    return 0;
+}
