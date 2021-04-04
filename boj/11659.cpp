8335237559cf9e06104bd082bd86450f363d4ffe
RedoC-github <coderlovemedia@gmail.com>
2021-04-04 23:03:14 +0900

    boj: 11659

--- boj/11659.cpp
+++ boj/11659.cpp
@@ -17,16 +17,14 @@ int main() {
     cin.tie(nullptr); cout.tie(nullptr);
 
     int n, m; cin >> n >> m;
-    vector<int> a(n);
-    cin >> a[0];
-    for (int i = 1; i < n+1; i++) {
+    vector<int> a(n+1);
+    for (int i = 0; i < n; i++) {
         int tmp; cin >> tmp;
-        a[i] = a[i-1] + tmp;
+        a[i+1] = a[i] + tmp;
     }
 
     while (m--) {
         int i, j; cin >> i >> j;
-        if (i < 2) cout << a[j-1] << endl;
-        else cout << a[j-1] - a[i-2] << endl;
+        cout << a[j] - a[i-1] << "\n";
     }
 }
