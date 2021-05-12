ec2085a650a4ffe70070b4e444b577382ed802b8
RedoC-github <coderlovemedia@gmail.com>
2021-05-12 16:21:53 +0900

    .

---
+++ boj/2668.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#pragma gcc optimize("O3")
+#pragma gcc optimize("Ofast") 
+#pragma gcc optimize("unroll-loops")
+typedef long long ll;
+using namespace std;
+typedef pair<int, int> P;
+
+void solve() {
+    int n; cin >> n;
+    vector<int> a(n);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    solve();
+    return 0;
+}
