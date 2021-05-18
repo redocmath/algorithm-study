9ec434ba3872e3cfff5df7bae9b99bee20b6005b
RedoC-github <coderlovemedia@gmail.com>
2021-05-18 21:12:02 +0900

    test

---
+++ boj/11066.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#pragma gcc optimize("O3")
+#pragma gcc optimize("Ofast") 
+#pragma gcc optimize("unroll-loops")
+typedef long long ll;
+using namespace std;
+typedef pair<int, int> P;
+
+int k, a[500];
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    int t; cin >> t;
+    while (t--) {
+        cin >> k;
+        for (int i = 0; i < k; i++) cin >> a[i];
+        solve();
+    }
+    return 0;
+}
